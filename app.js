cb6668c5c5746d90bce70463e64d044f7165e555
cyberdebb <deboramoreiracastro@gmail.com>
2024-12-03 22:15:44 -0300

    inicio framework7 e leitura do qrcode

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 var express = require('express');
-var app     = express();
+var app = express();
 
+app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 
+app.get('/', function(req,resp) {
+    resp.send();
+});
+
+app.listen(3000, function(){
+    console.log('Servidor rodando na porta 3000');
+});
+
+app.get(/^(.+)$/, function (req, res) {
+    try {
+      res.write("A pagina que vc busca nao existe")
+      res.end();
+    }
+    catch(e) {
+      res.end();
+    }    
+});
